2ad5d297298b862f01b089c3183b0a1d7ffd2e30
Juan Diego <chucjuan508@gmail.com>
2025-07-26 22:29:38 -0800

    1 commit Menu

---
+++ Actividad 09.py
@@ -0,0 +1,12 @@
+def saludar():
+    print("Bienvenido al catalogo de peliculas")
+saludar()
+while True:
+    print("\n ---MENU---")
+    print("1. Agregar peliculas")
+    print("2. Mostrar Peliculas registradas")
+    print("3. Buscar una pelicula por su genero")
+    print("4. Eliminar una pelicula")
+    print("5. Ver la estadistica del catalogo")
+    print("6. Salir del programa")
+    option = input("ingrese una opcion")
